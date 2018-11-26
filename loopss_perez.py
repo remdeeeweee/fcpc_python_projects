b78ae35ba6635cadac5efa08f5783152ee6dc927
remdeeeweee <45374710+remdeeeweee@users.noreply.github.com>
2018-11-27 07:47:42 +0800

    Rename loops_perez.py to loopss_perez.py

---
+++ loopss_perez.py
@@ -0,0 +1,18 @@
+for x in range(0,11,2):
+    print(x)
+for x in range(0,20,3):
+    print(x)
+for x in range(0,20,4):
+    print(x)
+for x in range(0,72,12):
+    print(x)
+
+#nested loop sample
+for y in range(1,11):
+    for z in range(1,11):
+        print(z * y, end='\t')
+    print()
+for y in range(10,21):
+    for z in range(1,11):
+        print(z * y, end='\t')
+    print()
