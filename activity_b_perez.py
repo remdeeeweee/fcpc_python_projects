09911d6cc63beaac5ac46de91b99620f9bb84411
remdeeeweee <45374710+remdeeeweee@users.noreply.github.com>
2018-11-27 11:22:46 +0800

    Create activity_b_perez.py

---
+++ activity_b_perez.py
@@ -0,0 +1,23 @@
+#number1
+print("number 1")
+for n in range(8):
+    print(2**n, end='\t')
+print()
+#number2
+print("number 2")
+for n in range (1,13):
+    if n % 4 == 0:
+      print("s", end ='\t')
+    else:
+      print(n, end ='\t')
+print()
+#number2
+print("number 2")
+for f in range (1,9):
+    if f % 8 == 1:
+        print(f, end = '\t')
+    else:
+        print("E", end='\t')
+
+
+
