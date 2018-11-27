125d87071aac39251a136c3f1a5a74e1452ddc82
remdeeeweee <45374710+remdeeeweee@users.noreply.github.com>
2018-11-27 09:33:51 +0800

    Create multiplication table perez.py

---
+++ multiplication table perez.py
@@ -0,0 +1,9 @@
+for x in range(1, 11):
+    for y in range(1, 11):
+       z = x * y
+
+       if z % 2 == 0:
+           print ("e", end = '\t')
+       else:
+           print(x*y, end = '\t')
+    print()
